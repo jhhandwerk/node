45952e3b2e74bcbf43f5b1ef99f2a25eb28f7b09
jonathanHandwerk <68844104+jonathanHandwerk@users.noreply.github.com>
2020-10-08 22:32:34 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) =>{
-    res.send("Welcome to the machine. Where have you been?");
-});
-app.get('/', (req, res) =>{
-    res.send("It's ok. We know where you've been...");
+    res.send("Welcome to the machine. Where have you been? It's ok, we know where you've been. You've been in the pipeline filling in time...");
 });
 const port = process.env.port||3000;
 app.listen(port, () =>{
