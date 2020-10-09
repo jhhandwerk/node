755b85eb7e6b9abb938acac44b06f19db6fcd4bb
jonathanHandwerk <68844104+jonathanHandwerk@users.noreply.github.com>
2020-10-09 23:39:16 +0900

    Update app.js

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const dt = require('./date_module');
 
 app.get('/', (req, res) =>{
     res.send("Welcome to the machine. Where have you been? It's ok, we know where you've been. You've been in the pipeline filling in time..");
+
+});
+app.get('/', (req, res) =>{
     res.write("The date and time are currently: " + dt.myDateTime());
 });
 const port = process.env.port||3000;
