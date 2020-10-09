c37d592af27734c07ece172d1dda5e6bb88b5371
thats a secret <56076506+durpidurpi@users.noreply.github.com>
2020-10-09 21:44:54 +0900

    variable port

--- app.js
+++ app.js
@@ -2,11 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) =>{
-    res.send("Welcome to the machine");
+    res.send("Welcome to the machine. Where have you been? It's ok, we know where you've been. You've been in the pipeline filling in time...");
 });
-
 const port = process.env.port||3000;
 app.listen(port, () =>{
     console.log("Wazzzappp");
 });
-//ssdefdfdf
