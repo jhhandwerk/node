014cb7f3c99dd00781d766c507a22b7436c263f7
thats a secret <56076506+durpidurpi@users.noreply.github.com>
2020-10-09 22:51:07 +0900

    test code

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
-const express = require('express');
-const app = express();
+var http = require('http');
+var dt = require('./date_module');
 
-
-app.get('/', (req, res) =>{
-    res.send("Welcome to the machine. Where have you been? It's ok, we know where you've been. You've been in the pipeline filling in time..");
-});
-const port = process.env.port||3000;
-app.listen(port, () =>{
-    console.log("Wazzzappp");
-});
+http.createServer(function (req, res){
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write("The date and time are currently: " + dt.myDateTime());
+    res.end();
+}).listen(8080);
