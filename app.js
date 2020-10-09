e3fc701ccad0e3de9d9ded4ad13e707fc786eb7b
thats a secret <56076506+durpidurpi@users.noreply.github.com>
2020-10-09 22:18:50 +0900

    back to original

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
 const express = require('express');
 const app = express();
-var http = require('http');
-var dt = require('./date_module');
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('The date and time is currently: ' + dt.myDateTime());
-    res.end();
-})
 
 app.get('/', (req, res) =>{
     res.send("Welcome to the machine. Where have you been? It's ok, we know where you've been. You've been in the pipeline filling in time..");
